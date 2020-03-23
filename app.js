6b4ee72f896e4ae088c69e06cb0d9cdf1c63dbd3
HabibMKhan <habibmkhan92@gmail.com>
2020-03-23 13:23:53 -0700

    assignment 2

--- app.js
+++ app.js
@@ -1,42 +1,24 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Enter Username</title><head>');
-    res.write('<body><h1>Hi Habib!</h1>');
-    res.write('<form action="/create-user" method="POST"><input type="text" name="username">');
-    res.write('<button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/users') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Usernames</title><head>');
-    res.write('<body><h1>Username List</h1>');
-    res.write('<ul><li>User 1</li></ul>');
-    res.write('</body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/create-user' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => { 
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const username = parsedBody.split('=')[1];
-      console.log('Username', username);
-    });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
-  }
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('Log 1');
+    next();
 });
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log('Log 2');
+    next();
+});
+
+app.use('/users', (req, res, next) => {
+    res.send('<h1>Users List:</h1><ul><li>User1</li><li>User2</li></ul>');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Welcome Home!</h1>');
+});
+
+
+app.listen(3000);
