c6d109ff382396041d895928cf3db984a504d2aa
Habib M. Khan <HabibMKhan92@gmail.com>
2020-04-06 20:40:30 -0700

    finish mongodb connected routes

--- models/product.js
+++ models/product.js
@@ -2,21 +2,26 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = id;
+    this._id = id ? new mongodb.ObjectID(id) : null;
+    this.userId = userId;
   }
 
   save() {
     const db = getDb();
     let dbOp;
     if (this._id) {
-      dbOp = db.collection('products').updateOne({_id: new mongodb.ObjectID(this._id)}, {$set: this});
+      dbOp = db
+        .collection('products')
+        .updateOne({_id: this._id}, {$set: this});
     } else {
-      dbOp = db.collection('products').insertOne(this);
+      dbOp = db
+        .collection('products')
+        .insertOne(this);
     }
     return dbOp
       .then(result => {
@@ -53,6 +58,17 @@ class Product {
       })
       .catch(err => {
         console.log(err);
+      });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db.collection('products').deleteOne({ _id: new mongodb.ObjectID(prodId) })
+      .then(result => {
+        console.log('Deleted');
+      })
+      .catch(err => {
+        console.log('err', err);
       });
   }
 }
