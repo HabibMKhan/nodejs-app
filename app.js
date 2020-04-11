dfb5188716d44f3544e2334d376bb7674eceaefa
Habib M. Khan <HabibMKhan92@gmail.com>
2020-04-11 00:36:34 -0700

    add csrf

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 const app = express();
 const store = new MongoDBStore({
   uri: process.env.ATLAS_URI,
   collection: 'sessions'
 });
+
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -30,6 +33,8 @@ app.use(session({
   saveUninitialized: false, 
   store
 }));
+
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session || !req.session.user) {
@@ -59,6 +64,12 @@ app.use((req, res, next) => {
     });  
   }
 	next();
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use('/admin', adminRoutes);
