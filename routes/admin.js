23c3b494c4210322877552f898ca3cbf2cec8c4e
Habib M. Khan <HabibMKhan92@gmail.com>
2020-04-18 23:24:55 -0700

    add route comments

--- routes/admin.js
+++ routes/admin.js
@@ -10,16 +10,19 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', isAuth, adminController.getAddProduct);
 
-// // /admin/products => GET
+// /admin/products => GET
 router.get('/products', isAuth, adminController.getProducts);
 
-// // /admin/add-product => POST
+// /admin/add-product => POST
 router.post('/add-product', isAuth, adminController.postAddProduct);
 
+// /admin/edit-product => GET
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
+// /admin/edit-product => POST
 router.post('/edit-product', isAuth, adminController.postEditProduct);
 
+// /admin/delete-product => POST
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
